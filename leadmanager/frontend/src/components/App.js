7ead7b1fc53694d895cc0c7be97d7261670bf1b1
ryancrumble <ryancrummypl@gmail.com>
2019-07-23 15:55:27 +1000

    Django Auth and React state auth completed

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,56 @@
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+import { Provider } from 'react-redux';
+import store from '../store';
+import { loadUser } from '../actions/auth';
+
+import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
+
+// Alert Options
+const alertOptions = {
+  timeout: 3000,
+  postion: 'top center'
+};
+
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Router>
+            <Fragment>
+              <Header />
+              <Alerts />
+              <div className='container'>
+                <PrivateRoute exact path='/' component={Dashboard} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
+              </div>
+            </Fragment>
+          </Router>
+        </AlertProvider>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
